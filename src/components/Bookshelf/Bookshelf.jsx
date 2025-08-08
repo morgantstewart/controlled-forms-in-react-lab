d57515a2109587815095410c4aa145ae83c2c249
morgantstewart <morgan@paysigil.com>
2025-08-08 10:58:06 -0400

    commit progress

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,9 +1,62 @@
 import { useState } from 'react';
 
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a Book</h3>
-    {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
+export default function Bookshelf() {
+  const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+  ]);
+
+  const [newBook, setNewBook] = useState({ title: '', author: '' });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewBook({ ...newBook, [name]: value });
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBooks([...books, newBook]);
+    setNewBook({ title: '', author: '' });
+  };
+
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Title: </label>
+            <input
+              type="text"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Author: </label>
+            <input
+              type="text"
+              name="author"
+              value={newBook.author}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+
+      <div className="bookCardsDiv">
+        {books.map((book, index) => (
+          <div key={index} className="bookCard">
+            <h4>{book.title}</h4>
+            <p>by {book.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
